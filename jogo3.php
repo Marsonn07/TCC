972c7e01efe7aed9258ced7217d6d39243e560e1
Marsonn07 <rafaelmarson.rafa@gmail.com>
2024-10-23 16:55:38 -0300

    Create jogo3.php

---
+++ jogo3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Musicando</title>
+    <link rel="icon" href="assets\logoMusicando.png" type="image/png">
+</head>
+<body>
+    <header>
+        <a class="logo"><img src="assets\logoMusicando.png" alt="Logo Musicando" class="logo-img"><h3>Musicando</h3></a>
+        <div class="Nome">
+            <h1>Jogo3</h1>
+        </div>
+        <br>
+        <div class="menu2">
+            <nav>
+                <ul>
+                    <li><button class="botao" onclick="scrollParaJogos()"><a href="jogos.php">Sair</a></button></li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+    <main>
+    <p>descrição</p>
+    <p>...</p>
+    <p>(jogo)</p>
+    </main>
+
+    <footer>
+        <p>Todos os direitos reservados @ 2024 | FEMA</p>
+    </footer>
+
+    <script>
+        function scrollParaSobre() {
+            document.getElementById('sobre').scrollIntoView({ behavior: 'smooth' });
+        }
+    </script>
+</body>
+</html>
